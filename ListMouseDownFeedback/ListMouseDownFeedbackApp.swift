b2d8dcabd7e51aaa2c64bd1b20bd384731acd1ff
seanmrich <sean.rich.register@mac.com>
2022-12-02 11:30:57 -0500

    Initial Commit

---
+++ ListMouseDownFeedback/ListMouseDownFeedbackApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListMouseDownFeedbackApp.swift
+//  ListMouseDownFeedback
+//
+//  Created by Sean Rich on 12/2/22.
+//
+
+import SwiftUI
+
+@main
+struct ListMouseDownFeedbackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
